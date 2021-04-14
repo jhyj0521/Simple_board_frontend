cef1d55d7f3c218013097454ca6dd054e792e95c
jhyj0521 <jhyj3636@gmail.com>
2021-04-14 12:11:58 +0900

    API 설정 공통화

--- src/service/memberService.js
+++ src/service/memberService.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+// API 설정 공통화
+const axiosService = axios.create({
+  baseURL: process.env.VUE_APP_API_URL
+});
+
 const register = data => {
-  const url = "http://localhost:8080/members/new";
-  return axios.post(url, data);
+  return axiosService.post("members/new", data);
 };
 
 export default { register };
