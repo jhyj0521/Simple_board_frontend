f924948513d584794c6b6e6254ddbe9195033aaa
jhyj0521 <jhyj3636@gmail.com>
2021-04-25 14:19:02 +0900

    관련 API 기능별 파일 분할

--- src/api/member.js
+++ src/api/member.js
@@ -1,16 +1,4 @@
-import axios from "axios";
-import { setInterceptors } from "./common/interceptors";
-
-// API 설정 공통화
-function createAxiosService() {
-  const axiosService = axios.create({
-    baseURL: process.env.VUE_APP_API_URL
-  });
-
-  return setInterceptors(axiosService);
-}
-
-const axiosService = createAxiosService();
+import { axiosService } from "./index";
 
 const register = data => {
   return axiosService.post("members/new", data);
